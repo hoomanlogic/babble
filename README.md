879fbbcc8f8e6bf5b62c7b28cfd0aed0a69466fe
geoffrey.floyd <geoffrey.floyd@hoomanlogic.com>
2015-04-10 15:49:19 -0400

    create new project with nlp logic for numbers

---
+++ README.md
@@ -0,0 +1,19 @@
+# Babelchip
+
+Babelchip is a JavaScript library for building user interfaces that understand humans.
+
+### Getting Started
+
+To get all the required packages, run the following commands in the root folder:
+
+    npm install
+
+### Building with Gulp
+    
+Run the following command to watch for changes and build automatically if the tests pass:
+
+    gulp watch
+    
+Manually run a build with the following command:
+
+    gulp build
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var gutil = require('gulp-util');
+var shell = require('gulp-shell');
+
+var onError = function (err) {  
+    gutil.beep();
+    console.log(err);
+};
+
+var buildPaths = ['src/*.js'];
+
+// TASK: Build
+gulp.task('build', function () {
+    return gulp.src(buildPaths)
+        .pipe(shell('mocha test'))
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', function () {
+    return gulp.src('')
+        .pipe(shell('mocha test'));
+});
+
+gulp.task('watch', function () {
+
+    // Watch for updates to javascript
+    var jsWatcher = gulp.watch(buildPaths, ['build']);
+    jsWatcher.on('change', function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running task...');
+    });
+    
+    // Watch for updates to test files
+    var testWatcher = gulp.watch('test/test.js', ['test']);
+    jsWatcher.on('change', function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running task...');
+    });
+});
