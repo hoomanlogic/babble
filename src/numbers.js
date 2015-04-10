b05f6e24f206a46cdbe76b4243b9359ba114dfba
Geoffrey Floyd <geoffrey.floyd@hoomanlogic.com>
2015-04-10 19:15:58 -0400

    change api to use a functional programming style refactor localization configuration out of the parse function

--- src/numbers.js
+++ src/numbers.js
@@ -9,68 +9,94 @@
      * This takes any string, locates groups of 
      * spelled out numbers and converts them to digits
      */
-    exports.digify = function (numberString, locale) {
-        var matches = exports.parseNums(numberString, locale);
-        for (var i = 0; i < matches.length; i++) {
-            numberString = numberString.replace(matches[i].text, matches[i].value);   
+    function digify () {
+        var input = this.input;
+        for (var i = 0; i < this.tokens.length; i++) {
+            input = input.replace(this.tokens[i].text, this.tokens[i].value);   
         }
-        return numberString;
+        return input;
     }
 
-    /**
-     * This takes any string, locates groups of 
-     * spelled out numbers and returns results
+    /*
+     * Create parsed results object and Bind functions to the parsed results for sugar
      */
-    exports.parseNums = function (numberString, locale) {
-        /*
-         * When expanding to support other locales
-         * ensure that numbers that contain the words of
-         * other numbers are entered AFTER the roots.
-         * When a RegEx is built, it reverses this list
-         * of mappings so that the ones toward the bottom
-         * are evaluated first
-         */
-        var map_en_us = {
-            'zero': 0,
-            'one': 1,
-            'two': 2,
-            'three': 3,
-            'four': 4,
-            'five': 5,
-            'six': 6,
-            'seven': 7,
-            'eight': 8,
-            'nine': 9,
-            'ten': 10,
-            'eleven': 11,
-            'twelve': 12,
-            'thirteen': 13,
-            'fourteen': 14,
-            'fifteen': 15,
-            'sixteen': 16,
-            'seventeen': 17,
-            'eighteen': 18,
-            'nineteen': 19,
-            'twenty': 20,
-            'thirty': 30,
-            'forty': 40,
-            'fifty': 50,
-            'sixty': 60,
-            'seventy': 70,
-            'eighty': 80,
-            'ninety': 90,
-            'hundred': 100,
-            'thousand': 1000,
-            'million': 1000000,
-            'billion': 1000000000
-        };
-        var joiners_en_us = ['',' ',' and ','-'];
+    function ParsedResult(input, tokens) {
+        this.input = input;
+        this.tokens = tokens;
+        this.digify = digify.bind(this);
+    }
 
-
-        /*
-         * Deutsch
-         */
-        var map_de_de = {
+    var Locales = {
+        'en-US': {
+            /*
+             * When expanding to support other locales
+             * ensure that numbers that contain the words of
+             * other numbers are entered AFTER the roots.
+             * When a RegEx is built, it reverses this list
+             * of mappings so that the ones toward the bottom
+             * are evaluated first
+             */
+            'numbers': {
+                'zero': 0,
+                'one': 1,
+                'two': 2,
+                'three': 3,
+                'four': 4,
+                'five': 5,
+                'six': 6,
+                'seven': 7,
+                'eight': 8,
+                'nine': 9,
+                'ten': 10,
+                'eleven': 11,
+                'twelve': 12,
+                'thirteen': 13,
+                'fourteen': 14,
+                'fifteen': 15,
+                'sixteen': 16,
+                'seventeen': 17,
+                'eighteen': 18,
+                'nineteen': 19,
+                'twenty': 20,
+                'thirty': 30,
+                'forty': 40,
+                'fifty': 50,
+                'sixty': 60,
+                'seventy': 70,
+                'eighty': 80,
+                'ninety': 90,
+                'hundred': 100,
+                'thousand': 1000,
+                'million': 1000000,
+                'billion': 1000000000
+            },
+            /**
+             * Joiners define what characters
+             * may separate words of the same number
+             */
+            'joiners': [
+                '',
+                ' ',
+                ' and ',
+                '-'
+            ],
+            /**
+             * Flippers define what characters between two number-words will
+             * cause a smaller number preceeding a larger number to be added 
+             * instead of treated as a modifier.
+             * Example: Germans write 'five and fifty' instead of 'fifty five'.
+             * So, this essentially 'flips' the behavior of processing
+             */
+            'flippers': [
+                
+            ]
+        }
+    };
+    
+    var CurrentLocale = Locales['en-US'];
+    
+    Locales['de-DE'] = {
+        'numbers': {
             'null': 0,
             'ein': 1,
             'eine': 1,
@@ -106,82 +132,125 @@
             'tausend': 1000,
             'million': 1000000,
             'milliarde': 1000000000
-        };
-        var joiners_de_de = ['',' ','und',' und ','-'];
+        },
+        'joiners': [
+            '',' ','und',' und ','-'
+        ],
+        'flippers': [
+            'und',' und '
+        ]
+    };
+    
+    /**
+     * This takes any string, locates groups of 
+     * spelled out numbers and returns results
+     */
+    exports.parse = function (input, locale) {
 
-        var map = null;
-        var joiners = null;
-
-        if (locale && locale === 'de-de') {
-            map = map_de_de;   
-            joiners = joiners_de_de;
+        /*
+         *
+         */
+        if (locale && Locales[locale]) {
+            locale = Locales[locale];
         } else {
-            map = map_en_us;   
-            joiners = joiners_en_us;
+            locale = CurrentLocale;   
         }
+        
 
+        // Build list of numbers and reverse so 
+        // it matches the 'teens' before the ones
         var names = [];
-        for (var name in map) {
+        for (var name in locale.numbers) {
           names.push(name);
         }
-
-        // reverse so it matches the teens before the ones
         names.reverse();
 
-        var digits = [];
-        var replaceNumbers = function (s, a, b, c) {
-            digits.push({
-                pos: b,
-                len: a.length,
-                value: map[s.toLowerCase()]
-            });
-
-            return map[s];
+        /*
+         * Function keeps the matches in order by the position
+         * so processing doesn't have to worry about sorting it
+         */
+        var insertByPosition = function (arr, obj) {
+            if (arr.length === 0 || arr[arr.length - 1].pos < obj.pos) {
+                arr.push(obj);   
+            } else {
+                for (var i = 0; i < arr.length; i++) {
+                    if (arr[i].pos > obj.pos) {
+                        arr.splice(i, 0, obj);
+                        break;
+                    }
+                }
+            }
         };
 
-        // build regex
+        /*
+         * Array for holding number matches
+         */
+        var matches = [];
+
+        /*
+         * Add numerical language matches
+         */
         var re = new RegExp('(' + names.join('|') + ')', 'gi');
-
-        // process string
-        numberString.replace(re, replaceNumbers);
-
-        if (digits.length === 0) {
-            return [];   
+        input.replace(re, function (s, a, b, c) {
+            insertByPosition(matches, {
+                pos: b,
+                len: a.length,
+                value: locale.numbers[s.toLowerCase()] || parseFloat(s)
+            });
+        });
+        
+        /*
+         * Add digit matches
+         */
+        re = /([+-]{0,1}\d+)/gi;
+        input.replace(re, function (s, a, b, c, d, e, f,g, h, i, j, k) {
+            insertByPosition(matches, {
+                pos: b,
+                len: a.length,
+                value: parseFloat(s)
+            });
+        });
+        
+        /*
+         * Add numerical language matches
+         */
+        if (matches.length === 0) {
+            return new ParsedResult(input, []); 
         }
 
         var result = [{ value: 0, text: '', segments: [0] }];
         var resultIndex = 0;
         var segmentIndex = 0;
         var previous = null;
-        for (var i = 0; i < digits.length; i++) {
+        for (var i = 0; i < matches.length; i++) {
 
             // first lets check to see what characters
             // are joining this result with the last result
             if (previous) {
-                var joiner = numberString.slice(previous.pos + previous.len, digits[i].pos);
-                if (joiners.indexOf(joiner) === -1) {
+                var joiner = input.slice(previous.pos + previous.len, matches[i].pos);
+                if (locale.joiners.indexOf(joiner) === -1) {
                     result[resultIndex].value = result[resultIndex].segments.reduce(function (prev, next) { return (prev || 0) + next; });
-                    result[resultIndex].text = numberString.slice(result[resultIndex].pos, previous.pos + previous.len);
+                    result[resultIndex].text = input.slice(result[resultIndex].pos, previous.pos + previous.len);
                     result.push({ value: 0, text: '', segments: [0] });
                     resultIndex++;
                     segmentIndex = 0;
                     previous = null;
-                    result[resultIndex].pos = digits[i].pos;
+                    result[resultIndex].pos = matches[i].pos;
                 }
             } else {
-                result[resultIndex].pos = digits[i].pos;
+                result[resultIndex].pos = matches[i].pos;
             }
 
-            if (previous && result[resultIndex].segments[segmentIndex] < digits[i].value) {
+            if (previous && result[resultIndex].segments[segmentIndex] < matches[i].value) {
 
                 // check previous segments (todo: recursive)
-                if (segmentIndex > 0 && result[resultIndex].segments[segmentIndex - 1] < digits[i].value) {
+                if (segmentIndex > 0 && result[resultIndex].segments[segmentIndex - 1] < matches[i].value) {
 
                     // traverse backwards until the end or sum is greater than current value
                     var segmentsTally = 0;
                     var splitter = 0;
                     for (var j = result[resultIndex].segments.length - 1; j > -1; j--) {
-                        if (segmentsTally + result[resultIndex].segments[j] > digits[i].value) {
+                        if (segmentsTally + result[resultIndex].segments[j] > matches[i].value) {
                             splitter = j + 1;
                             break;
                         }
@@ -190,31 +259,34 @@
 
                     result[resultIndex].segments.splice(splitter, result[resultIndex].segments.length);
                     segmentIndex = splitter;
-                    result[resultIndex].segments.push(segmentsTally * digits[i].value);
+                    result[resultIndex].segments.push(segmentsTally * matches[i].value);
 
                 } else {
-                    // german language puts the 1s before the 10s
-                    // will eventually push this option back on the locale
-                    // to keep this clean of locale specific logic
-                    if (locale && locale === 'de-de' && numberString.slice(previous.pos + previous.len, digits[i].pos).trim() === 'und') {
-                        result[resultIndex].segments[segmentIndex] += digits[i].value;
+                    // German language puts the 1s before the 10s, likely other languages do as well
+                    if (locale.flippers.indexOf(input.slice(previous.pos + previous.len, matches[i].pos)) > -1) {
+                        result[resultIndex].segments[segmentIndex] += matches[i].value;
                     } else {
-                        result[resultIndex].segments[segmentIndex] = result[resultIndex].segments[segmentIndex] * digits[i].value;
+                        result[resultIndex].segments[segmentIndex] = result[resultIndex].segments[segmentIndex] * matches[i].value;
                     }
                 }
-            } else if (previous && result[resultIndex].segments[segmentIndex] > digits[i].value) {
-                result[resultIndex].segments.push(digits[i].value);
+            } else if (previous && result[resultIndex].segments[segmentIndex] > matches[i].value) {
+                result[resultIndex].segments.push(matches[i].value);
                 segmentIndex++;
             } else {
-                result[resultIndex].segments[segmentIndex] += digits[i].value;
+                result[resultIndex].segments[segmentIndex] += matches[i].value;
             }
 
-            previous = digits[i];
+            previous = matches[i];
         }
         result[resultIndex].value = result[resultIndex].segments.reduce(function (prev, next) { return (prev || 0) + next; });
-        result[resultIndex].text = numberString.slice(result[resultIndex].pos, previous.pos + previous.len);
+        result[resultIndex].text = input.slice(result[resultIndex].pos, previous.pos + previous.len);
 
-        return result;   
+
+        
+        /*
+         * Create parsed results object and Bind functions to the parsed results for sugar
+         */
+        return new ParsedResult(input, result);
     }
 
 }(typeof exports === 'undefined' ? this['numbers'] = {}: exports));
--- test/test.js
+++ test/test.js
@@ -1,17 +1,27 @@
-var numbers = require('../src/numbers.js');
-
 var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
 
+/**
+ * Numbers Testing
+ */
+var numbers = require('../src/numbers.js');
+
 describe('numbers digify', function() {
     
     it('should parse "one million two hundred fifty one thousand three hundred and sixty five"', function() {
-        numbers.digify('one million two hundred fifty one thousand three hundred and sixty five').should.equal('1251365');
+        numbers.parse('one million two hundred fifty one thousand three hundred and sixty five').digify().should.equal('1251365');
     })
     
     it('should parse "one million two hundred fifty one thousand three hundred and sixty five cats and sixty five dogs"', function() {
-        numbers.digify('one million two hundred fifty one thousand three hundred and sixty five cats and sixty five dogs').should.equal('1251365 cats and 65 dogs');
+        numbers.parse('one million two hundred fifty one thousand three hundred and sixty five cats and sixty five dogs').digify().should.equal('1251365 cats and 65 dogs');
+    })
+        
+    it('should parse "eine million sechs hundertdreiundfünfzigtausend eins katze und drei hundert siebzig hunde"', function() {
+        numbers.parse('eine million sechs hundertdreiundfünfzigtausend eins katze und drei hundert siebzig hunde', 'de-DE').digify().should.equal('1653001 katze und 370 hunde');
     })
     
+    it('should parse "50 cats flew twenty 2 miles per hour past five dogs"', function() {
+        numbers.parse('50 cats flew twenty 2 miles per hour past five dogs').digify().should.equal('50 cats flew 22 miles per hour past 5 dogs');
+    })
 })
